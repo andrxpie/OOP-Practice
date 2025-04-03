4f5b3029f4f10ffc90e2867388efdacfcfc85185
Admin <Admin@DESKTOP-JP76EFQ>
2025-04-03 21:14:50 +0300

    Add project files.

---
+++ Practice №1/Practice №1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+    int testVar = 3, testRes;
+    testRes = 3 / testVar;
+    std::cout << "result: " << testRes << "\n";
+
+    testRes = 3 / testVar;
+    std::cout << "result: " << testRes << "\n";
+}
